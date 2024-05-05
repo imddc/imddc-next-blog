96268bf5d4d07ace68f83b8dda50996e0ad3330a
Du Cheng <easyhoo2426@163.com>
2024-05-05 14:06:55 +0800

    feat(motion): framer motion test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+'use client'
+
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  type Variants
+} from 'framer-motion'
+import { useState } from 'react'
+
+export default function RootPage() {
   return (
-    <>
-      <main>this is main page</main>
-    </>
+    <div className="w-screen h-screen flex-center">
+      <motion.div
+        className="rounded-full bg-orange-300 w-52 h-52"
+        animate={{ x: [0, 100, 0] }}
+      />
+      <motion.circle
+        className="bg-orange-300 w-52 h-52"
+        cx={500}
+        animate={{ cx: [null, 100, 200] }}
+        transition={{ duration: 3, times: [0, 0.2, 1] }}
+      />
+    </div>
   )
 }
