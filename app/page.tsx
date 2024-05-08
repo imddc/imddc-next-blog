2b7f3c5ca5a40d4e9268b356ed37cf29efeea633
Du Cheng <easyhoo2426@163.com>
2024-05-08 09:53:57 +0800

    feat(scroll): global css

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,6 @@ export default function RootPage() {
   return (
     <SectionContextProvider>
       <Header />
-
-      {links.map((link) => (
-        <div
-          key={link.hash}
-          id={link.name}
-          className="h-screen w-full bg-transparent"
-        >
-          {link.name}
-        </div>
-      ))}
     </SectionContextProvider>
   )
 }
