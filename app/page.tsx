4576e1aa2c66b990f2bcfcbf57be00267e11b9ee
Du Cheng <easyhoo2426@163.com>
2024-05-08 08:50:14 +0800

    feat(header): header top and motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { clsx } from 'clsx'
-import { motion, type Variants } from 'framer-motion'
+import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { useState } from 'react'
 
@@ -21,10 +21,12 @@ export default function RootPage() {
   return (
     <>
       <motion.div
-        className="fixed top-5 left-1/2 -translate-x-1/2 bg-black p-2 rounded-full"
+        className="fixed left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-40
+        shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: '-50%', opacity: 0 }}
         animate={{ y: 0, x: '-50%', opacity: 1 }}
-      >
+      />
+      <nav className="flex fixed top-[0.8rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.9rem] sm:h-[initial] sm:py-0">
         <motion.ul className="flex items-center gap-4 text-gray-500">
           {links.map((link) => (
             <motion.li
@@ -35,6 +37,7 @@ export default function RootPage() {
               onClick={() => handleClickItem(link.name)}
             >
               <Link
+                href={link.hash}
                 className={clsx(
                   'relative p-2 z-999 font-bold hover:text-gray-200 transition',
                   {
@@ -42,12 +45,11 @@ export default function RootPage() {
                       activeSection === link.name
                   }
                 )}
-                href={link.hash}
-                onClick={() => setActiveSection(link.name)}
+                onClick={() => handleClickItem(link.name)}
               >
                 {link.name}
                 {activeSection === link.name && (
-                  <motion.span
+                  <motion.div
                     className="absolute inset-0 bg-gray-100 rounded-full -z-10"
                     layoutId="activeSection"
                     transition={{ type: 'spring', stiffness: 380, damping: 30 }}
@@ -57,7 +59,7 @@ export default function RootPage() {
             </motion.li>
           ))}
         </motion.ul>
-      </motion.div>
+      </nav>
     </>
   )
 }
