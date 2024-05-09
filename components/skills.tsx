a2404bc4d7155660443775e27651016730afbc88
Du Cheng <easyhoo2426@163.com>
2024-05-09 12:06:11 +0800

    feat(skills): add animation

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,25 +1,42 @@
 'use client'
 
-import { clsx } from 'clsx'
-import React, { useEffect } from 'react'
+import { motion } from 'framer-motion'
+import React from 'react'
 import { skills } from '~/lib/data'
 
 const Skills = () => {
   return (
-    <section className="max-w-[56rem] mx-auto mb-10">
-      <h2 className="text-3xl mb-10 text-center">My Skills</h2>
+    <motion.section
+      className="max-w-[56rem] mx-auto mb-10"
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+    >
+      <h2 className="section-title">My Skills</h2>
 
       <main className="flex-center flex-wrap gap-4">
-        {skills.map((skill) => (
-          <span
+        {skills.map((skill, idx) => (
+          <motion.span
             className="p-4 py-2 border border-white border-opacity-40 bg-white rounded-md cursor-pointer"
+            initial={{
+              y: 200,
+              opacity: 0
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: {
+                delay: idx * 0.02
+              }
+            }}
+            viewport={{ once: true }}
+            whileInView={'animate'}
             key={skill}
           >
             {skill}
-          </span>
+          </motion.span>
         ))}
       </main>
-    </section>
+    </motion.section>
   )
 }
 
